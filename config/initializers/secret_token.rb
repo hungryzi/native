bb6be901b593a56d44ad3d1c33d611241a78d247
Zi <hungryzi@gmail.com>
2012-12-24 22:41:23 +0800

    fresh rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookworm::Application.config.secret_token = 'f2ade295fe6e52b0372163e5d39937a06d7e4f6d58c12e095340aa9bbb323f87668e4dfee7f3b60e8c1a020a93630ccaf42f0c3afd8c50040bf6998dc159dad1'
