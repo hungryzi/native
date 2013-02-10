20d4ce767399a25760c1f1a581bd66025863e69f
Zi <hungryzi@gmail.com>
2013-02-10 11:27:44 +0700

    sorts words in ascending order of # of occurrences

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @text = params['text'] || ''
-    @sorted = word_occurrences.sort_by { |k, v| -v }
     word_occurrences = Parser.count_occurrences(Parser.remove_numbers(Parser.split_words(@text)))
+    @sorted = word_occurrences.sort_by { |k, v| v }
   end
 end
